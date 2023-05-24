d53c50d5cdfa551882577cc63591e2311d3c66d2
FelipeESchmidt <schmidtfe2@gmail.com>
2023-05-23 22:17:41 -0300

    feat: adjusting historic item and adding new colors/strings

--- app/src/main/java/unijui/edu/br/shopwise/HistoricScreen.java
+++ app/src/main/java/unijui/edu/br/shopwise/HistoricScreen.java
@@ -2,16 +2,40 @@ package unijui.edu.br.shopwise;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HistoricScreen extends AppCompatActivity {
 
+    private LinearLayout verticalLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_historic);
+
+        verticalLayout = findViewById(R.id.verticalItems);
+        renderHistoricList();
+        renderHistoricList();
+    }
+
+    public void renderHistoricList(){
+        LayoutInflater inflater = LayoutInflater.from(this);
+
+        //verticalLayout.removeAllViews();
+        View customView = inflater.inflate(R.layout._item_historic, null);
+        TextView textViewNome = customView.findViewById(R.id.listName);
+        textViewNome.setText("Lista de teste");
+
+        TextView textViewQtd = customView.findViewById(R.id.quantityItem);
+        textViewQtd.setText("2");
+
+        verticalLayout.addView(customView);
     }
 
     public void onListItemsClick(View view){
