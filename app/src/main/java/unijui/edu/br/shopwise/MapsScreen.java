40237d2710831c8722b383286f86b9f99d674bad
visitante <visitante@IJDCEENG14.unijui.intranet>
2023-06-06 20:55:30 -0300

    mapa acho que pegou

--- app/src/main/java/unijui/edu/br/shopwise/MapsScreen.java
+++ app/src/main/java/unijui/edu/br/shopwise/MapsScreen.java
@@ -4,9 +4,11 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.common.annotation.NonNullApi;
@@ -75,6 +77,16 @@ public class MapsScreen extends FragmentActivity implements OnMapReadyCallback {
         //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
+    public void onHistoricClick(View view){
+        Intent intent = new Intent(MapsScreen.this, HistoricScreen.class);
+        startActivity(intent);
+    }
+
+    public void onListItemsClick(View view){
+        Intent intent = new Intent(MapsScreen.this, MainActivity.class);
+        startActivity(intent);
+    }
+
     public void getCurrentLocation() {
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -91,6 +103,7 @@ public class MapsScreen extends FragmentActivity implements OnMapReadyCallback {
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                     @Override
                     public void onSuccess(Location location) {
+                        System.out.println(location);
                         // Got last known location. In some rare situations this can be null.
                         if(location != null){
                             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
